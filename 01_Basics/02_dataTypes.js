9b07d9f5df4729f24c3e70b5b1999b7faaddeaed
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-14 05:47:26 +0000

    datatype

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert( 3+3 ) we are using node js not browser
+console.log(3
+    +3) //code readability should be high
+
+    console.log("Aradhya")
+
+    let name = "Aradhya"
+    let age = 20
+    let isLoggedIn = false
+    let state;
+
+    //number => 2 to the power 53
+    //bigint 
+    //string => true/false
+    //null => standalone value
+    //undefined =>
+    //symbol => unique
+    
+    //object
+
+    console.log(typeof null);//null is a object type
+    console.log(typeof undefined);// undefined =>undefined type hi h
