141b8118afe4621d51b239ad34bc81922c476592
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-14 06:23:35 +0000

    conver

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc"//score =>null then 0 || score =>undefined then NaN(not a number) || score =>true or false then 1 or 0 
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+  
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 0 =>false  1 => true  
+// " " => false  "aradhya" => true
+
+let someNumber = 33;
+ let stringNumber = String(someNumber)
+
+console. log (stringNumber) ;
+console. log (typeof stringNumber);
