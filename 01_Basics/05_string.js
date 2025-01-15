2f7ee312ab981eb89629c52e0d9a473b88462047
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-15 14:38:54 +0000

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,16 @@
+const name = "Aradhya"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");  // however this is not the perfect method
+ 
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('aradhyah')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('5'));
+//console.log(gameName.indexOf('d'));
