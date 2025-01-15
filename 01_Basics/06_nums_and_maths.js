46878283b590f51b7ac2e9c035e931d935e2b8b8
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-15 16:05:22 +0000

    num

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,9 @@
+const score = 555
+console.log(score);
+
+const info = new Number(123)
+console.log(info);   // => it will display the type (number) 
+
+console.log(info.toString());   // => it will change the value to string
+console.log(info.toString().length);  // => it will give the length of the string
+console.log(info.toFixed(2));  // => 100.00
