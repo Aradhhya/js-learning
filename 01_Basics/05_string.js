a44b05caff168caa6da897e12049cb6f14e0cd6b
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-15 15:54:28 +0000

    string

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -14,3 +14,18 @@ const gameName = new String('aradhyah')
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt('5'));
 //console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0,4) //doesnot include negative
+console.log(newString);
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString);
+
+const newStringOne = "   Aman    "
+
+console.log ( newStringOne);
+console.log ( newStringOne.trim( ));
+
+const url = "https://aradhya.com/aradhya%20sharma"
+console.log(URL.replace('%20' , '-'))  //[search, replace]
+
+console.log(url.includes('anything')) // =>false(not present)
