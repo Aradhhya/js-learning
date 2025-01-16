277c0d92404b269b3f095f0f36622e9414c78698
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-16 17:27:24 +0000

    array

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,32 @@
+      // Array
+
+      const arr = [1,4,7,8,9]   // => type of arrr : object
+      const myHeros = [ "shaktiman " , "amitabh bachan"]
+
+      const arr2 = new Array( 1,2,3,4)
+       // console.log(arr2[3]);
+
+       // Array methods
+
+        // arr.push(14)
+        // arr.push(67)   => add the value but at last
+        // arr.pop()       => deletes the value but from last
+        // console.log(arr);
+
+       //  arr.unshift(100)  // => add the element from the first index
+       //  arr.shift()     // => removes the element from first index
+
+        // console.log(arr.includes(9));  // => tells whether the given number is present or not if not then returns false
+         //console.log(arr.indexOf(7));  // => 2
+
+        // const newArr = arr.join()  // => converts the array into string 
+         //console.log(newArr);
+         //console.log( typeof newArr);  // => string
+
+             /// slice   , splice
+              
+             console.log(" A" , arr)
+             const myn1 = arr.slice(1,8)
+             console.log(myn1);
+         
+
