208a5ee98c4481897337c3aed6b9cade135a4983
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-16 14:47:37 +0000

    date

--- 01_Basics/07_dates.js
+++ 01_Basics/07_dates.js
@@ -3,7 +3,7 @@
   let myDate = new Date()  // => here myDate is a type of object
   console.log(myDate.toString());
   console.log(myDate.toDateString());
-  console.log(myDate.toLocalString());
+  console.log(myDate.toLocaleString());
   console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2025, 0, 23)
@@ -11,6 +11,19 @@
    
 
   let myCreatedDate = new Date (" 2025-02-05")
-  console.log(myCreatedDate.toLocalString());
+  console.log(myCreatedDate.toLocaleString());
 
-  let myTimeStamp = Date.now
+  let myTimeStamp = Date.now()
+  console.log(myTimeStamp) ;
+  console.log(myCreatedDate.getTime()) ;
+  console.log(Math.floor(Date.now()/1000));
+  let newDate = new Date()
+  console.log(newDate);
+  console.log(newDate.getMonth()+1);  // Month 
+  console.log(newDate.getDay());  // day 
+   
+console.log(`The day is ${newDate.getDay()} and the time is ${newDate}`)
+
+newDate.toLocaleString('default',{
+     weekday: "long",
+})  // press ctrl + space
