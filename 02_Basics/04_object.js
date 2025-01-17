9db5e792828efab7790213d13389225667d2d02e
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-17 16:01:53 +0000

    object

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,24 @@
+ // const tinderUser = new Object()   // => it works a singleton
+ const tinderUser = {}   // woks as non-singleton
+
+ tinderUser.Id = "123klmn"
+ tinderUser.name = "Ram"
+ tinderUser.isLoggedIn = false
+   // console.log(tinderUser);
+
+   const regularUser = {
+    email: "aradhya@gmail.com",
+    fullName : {
+        userfullName :{
+            firstName: "Aradhya",
+            lastName: "Sharma",
+        }
+    }
+   }
+     // console.log(regularUser.fullName.userfullName.firstName)
+
+     const obj1 ={ 1: "a" , 2: "b"}
+     const obj2 = { 3: "a" , 4: "b"}
+      // const obj3 = {obj1 ,obj2}
+      const obj3 = 
+     console.log(obj3)
