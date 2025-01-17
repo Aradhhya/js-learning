e28b09e83ba064246dd12fddf7904a661d99e4f0
Aradhhya <164992112+Aradhhya@users.noreply.github.com>
2025-01-17 17:43:09 +0000

    basics of function

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,16 @@
+function myName(){
+    console.log("A")
+    console.log("R") 
+    console.log("A")
+    console.log("D")
+    console.log("H")
+    console.log("Y")
+    console.log("A")
+}
+      // myName()
+      function addTwoNumbers(num1 , num2){  // function parameters
+        console.log( num1 + num2)
+      }
+      addTwoNumbers(3 , 4)   // value which is passed during function call is called arguments  //  => 7
+      addTwoNumbers( 3, "4")  // => 34
+      addTwoNumbers( 3, "a")  // => 3a
